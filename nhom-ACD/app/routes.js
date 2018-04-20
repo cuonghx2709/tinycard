6a4920ce3c79f99110f40adcfbb06bda1ce0f516
kedumuc1712 <sieutroma7k51@gmail.com>
2018-04-20 21:39:16 +0700

    Xoa file thua va sua lai routes

--- nhom-ACD/app/routes.js
+++ nhom-ACD/app/routes.js
@@ -1,15 +1,15 @@
 module.exports = function(app, passport) {
     const lessonRouter = require('../modules/api/lesson/router')
     const cardRoter = require('../modules/api/cardlesson/router')
-   
 
-// normal routes ===============================================================
+
+    // lesson routes
     app.use('/api/lesson', lessonRouter)
     app.use('/api/cardlesson', cardRoter)
-  
+
     // show the home page (will also have our login links)
     app.get('/', function(req, res) {
-        res.render('home.ejs');
+        res.render('index.ejs');
     });
 
     // show the lesson page 
@@ -18,14 +18,12 @@ module.exports = function(app, passport) {
     })
 
     app.get('/completelesson', (req, res) => {
-        res.render('1-7.ejs')
+        res.render('complete.ejs')
     })
-    
 
-    // PROFILE SECTION =========================
-    app.get('/1', isLoggedIn, function(req, res) {
-        res.render('1.ejs', {
-            user : req.user
+    app.get('/home', isLoggedIn, function(req, res) {
+        res.render('home.ejs', {
+            user: req.user
         });
     });
 
@@ -35,90 +33,88 @@ module.exports = function(app, passport) {
         res.redirect('/');
     });
 
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
-
-    // locally --------------------------------
-        // LOGIN ===============================
-        // show the login form
-        app.get('/login', function(req, res) {
-            res.render('home.ejs', { message: req.flash('loginMessage') });
+    // login
+    app.get('/login', function(req, res) {
+        res.render('home.ejs', {
+            message: req.flash('loginMessage')
         });
+    });
 
-        // process the login form
-        app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/1', // redirect to the secure profile section
-            failureRedirect : '/', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
 
-        // SIGNUP =================================
-        // show the signup form
-        app.get('/signup', function(req, res) {
-            res.render('signup.ejs', { message: req.flash('signupMessage') });
+    // SIGNUP =================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+        res.render('signup.ejs', {
+            message: req.flash('signupMessage')
         });
+    });
 
-        // process the signup form
-        app.post('/signup', passport.authenticate('local-signup', {
-            successRedirect : '/1', // redirect to the secure profile section
-            failureRedirect : '/signup', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-        }));
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/home', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
 
     // twitter --------------------------------
 
-        // send to twitter to do the authentication
-        app.get('/auth/twitter', passport.authenticate('twitter', { scope : 'email' }));
+    // send to twitter to do the authentication
+    app.get('/auth/twitter', passport.authenticate('twitter', {
+        scope: 'email'
+    }));
 
-        // handle the callback after twitter has authenticated the user
-        app.get('/auth/twitter/callback',
-            passport.authenticate('twitter', {
-                successRedirect : '/1',
-                failureRedirect : '/'
-            }));
+    // handle the callback after twitter has authenticated the user
+    app.get('/auth/twitter/callback',
+        passport.authenticate('twitter', {
+            successRedirect: '/home',
+            failureRedirect: '/'
+        }));
 
 
     // google ---------------------------------
 
-        // send to google to do the authentication
-        app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+    // send to google to do the authentication
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
-        // the callback after google has authenticated the user
-        app.get('/auth/google/callback',
-            passport.authenticate('google', {
-                successRedirect : '/1',
-                failureRedirect : '/'
-            }));
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            successRedirect: '/home',
+            failureRedirect: '/'
+        }));
 
-// =============================================================================
-// AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
-// =============================================================================
+    // twitter
 
-    // twitter --------------------------------
+    app.get('/connect/twitter', passport.authorize('twitter', {
+        scope: 'email'
+    }));
 
-        // send to twitter to do the authentication
-        app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
-
-        // handle the callback after twitter has authorized the user
-        app.get('/connect/twitter/callback',
-            passport.authorize('twitter', {
-                successRedirect : '/1',
-                failureRedirect : '/'
-            }));
+    app.get('/connect/twitter/callback',
+        passport.authorize('twitter', {
+            successRedirect: '/home',
+            failureRedirect: '/'
+        }));
 
 
     // google ---------------------------------
 
-        // send to google to do the authentication
-        app.get('/connect/google', passport.authorize('google', { scope : ['profile', 'email'] }));
+    app.get('/connect/google', passport.authorize('google', {
+        scope: ['profile', 'email']
+    }));
 
-        // the callback after google has authorized the user
-        app.get('/connect/google/callback',
-            passport.authorize('google', {
-                successRedirect : '/1',
-                failureRedirect : '/'
-            }));
+    app.get('/connect/google/callback',
+        passport.authorize('google', {
+            successRedirect: '/home',
+            failureRedirect: '/'
+        }));
 
 };
 
